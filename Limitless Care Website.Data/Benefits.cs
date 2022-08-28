c85cdb58218485566dfcbecda43904ad9cb68e8d
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-08-29 01:41:20 +0200

    Update Benefits.cs

--- Limitless Care Website.Data/Benefits.cs
+++ Limitless Care Website.Data/Benefits.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Limitless_Care_Website.Data
 {
-    internal class Benefits
+    public class Benefits
     {
         [Key]
         public int Id { get; set; }
         public int InPatientId { get; set; }
+        [ForeignKey("InPatient_Id")]
         public int OutPatientId { get; set; }
+        [ForeignKey("OutPatient_Id")]
         public int OthersId { get; set; }
+        [ForeignKey("Others_Id")]
         public int SpecialBenefitsId { get; set; }
+        [ForeignKey("SpecialBenefits")]
     }
 }
