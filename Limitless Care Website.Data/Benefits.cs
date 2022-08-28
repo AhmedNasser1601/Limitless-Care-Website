4651d562a75e03ef563997f2d874c89002ef57ba
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-08-29 01:17:35 +0200

    Update
    Add Benefits and Requests

---
+++ Limitless Care Website.Data/Benefits.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Limitless_Care_Website.Data
+{
+    internal class Benefits
+    {
+        [Key]
+        public int Id { get; set; }
+        public int InPatientId { get; set; }
+        public int OutPatientId { get; set; }
+        public int OthersId { get; set; }
+        public int SpecialBenefitsId { get; set; }
+    }
+}
